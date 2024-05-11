ec9c5f65a5fbbfe2a7bbc95fa92831a04fe04baa
sidPhoenix17 <siddarth@drdroid.io>
2024-05-11 16:18:08 +0530

    first version of the changelog

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,63 @@
+import streamlit as st
+import re
+from utils.github_data_fetch import fetch_prs_merged_between_dates, fetch_commits_from_prs
+from utils.summarisation import gpt_inference_changelog, extract_messages_from_commits
 
+st.title('Changelog Auto-Generator')
+st.text('This app generates a changelog based on the commits in a repository.')
+
+def validate_github_url(url):
+    pattern = r'^https:\/\/github\.com\/([A-Za-z0-9_.-]+)\/([A-Za-z0-9_.-]+)$'
+    match = re.match(pattern, url)
+    if match:
+        owner = match.group(1)
+        repository_name = match.group(2)
+        return owner, repository_name
+    else:
+        return None, None
+
+repository = st.text_input('Repository URL', 'https://github.com/DrDroidLab/playbooks')
+owner, repo = validate_github_url(repository)
+if owner is None or repo is None:
+    st.error('Invalid repository URL. Switching to default repository.')
+    owner, repo = 'DrDroidLab', 'playbooks'
+
+start_date, end_date = st.columns(2)
+start_date = start_date.date_input('Start Date')
+end_date = end_date.date_input('End Date')
+
+only_main_prs = st.checkbox('Only Include PRs merged into main branch', value=True)
+if (st.checkbox('Change main branch name', value=False)):
+    main_branch = st.text_input('Main branch name', 'main')
+else:
+    main_branch = 'main'
+
+st.text("Review the details and click the button below to generate the changelog.")
+st.markdown(f"Repository: {owner}/{repo}")
+st.markdown(f"Date Range: {start_date} to {end_date}")
+st.markdown(f"Main Branch: {main_branch}")
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+
+st.button('Generate Changelog', key='generate_button', on_click=click_button)
+
+if(st.session_state.clicked):
+    st.session_state.clicked = False
+    st.text("Fetching PRs...")
+    prs = fetch_prs_merged_between_dates(owner, repo, start_date, end_date, main_branch)
+    st.text(f"Fetched {prs.shape[0]} PRs")
+    st.text("Fetching commits...")
+    commits = fetch_commits_from_prs(prs, owner, repo)
+    st.text(f"Fetched {commits.shape[0]} commits")
+    st.text("Extracting commit messages...")
+    prompt_body = extract_messages_from_commits(commits)
+    st.text("Commit messages extracted")
+    st.text("Generating changelog...")
+    changelog = gpt_inference_changelog(prompt_body)
+    st.text("Changelog generated")
+    st.markdown(changelog)
+    # st.button('Generate Changelog', key='generate_button', on_click='enable')
---
+++ utils/github_data_fetch.py
@@ -0,0 +1,86 @@
+#Assumes github_api_key is set in the environment variables 
+
+import requests
+import pandas as pd
+import time
+from tqdm import tqdm
+from datetime import datetime, timedelta
+import os
+
+def github_api_call(url_suffix, owner, repo, params = {}):
+
+    # Set the bearer token
+    token = os.getenv('github_api_key',None)
+    if token is None:
+        raise ValueError("Please set the 'github_api_key' environment variable")
+
+    # Set the headers for the API request
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github+json"
+    }
+
+    url = f'https://api.github.com/repos/{owner}/{repo}/{url_suffix}'
+    print(url)
+    time.sleep(1)  # Add a sleep of 1 second
+    response = requests.get(url, params=params, headers=headers)
+    return response
+
+def fetch_commits_from_prs(prs, owner, repo):
+    commit_data = []
+    for i in prs.index:
+        pr_number = prs.loc[i, 'number']
+        time.sleep(1)
+        commits = fetch_commits_from_pr(pr_number, owner, repo)
+        for commit in commits:
+            commit_data.append({
+                'PR Number': pr_number,
+                'PR Title': prs.loc[i,'title'],
+                'Commit SHA': commit['sha'],
+                'Commit Message': commit['commit']['message']
+            })
+    df_commit_data = pd.DataFrame(commit_data)
+    return df_commit_data
+
+def fetch_commits_from_pr(pr_number, owner, repo):
+    # Construct the API URL for fetching the commits of the PR
+    commits_url_suffix = f"pulls/{pr_number}/commits"
+    # Make the API call to fetch the commits
+    response = github_api_call(commits_url_suffix, owner, repo)
+
+    # Check if the API call was successful
+    if response.status_code == 200:
+        commits = response.json()
+        return commits
+    else:
+        print(f"Failed to fetch commits from PR {pr_number}. Status code: {response.status_code}")
+        return None
+    
+def fetch_prs_merged_between_dates(owner, repo, start_date, end_date, main_branch='main'):
+    # Format the date range in ISO 8601 format
+    start_date_str = start_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+    end_date_str = end_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+    # Construct the API URL for fetching the PRs merged between the dates
+    prs_url_suffix = "pulls"
+    params = {
+        "state": "closed",
+        "base": main_branch,
+        "sort": "updated",
+        "direction": "desc",
+        "per_page": 100    
+        }
+    # Make the API call to fetch the PRs
+    response = github_api_call(prs_url_suffix, owner, repo, params)
+
+    # Check if the API call was successful
+    if response.status_code == 200:
+        prs = response.json()
+        # Create a DataFrame from the merged PRs
+        df = pd.DataFrame(prs)
+        df = df[~df['merged_at'].isnull()]
+        df['merged_at'] = pd.to_datetime(df['merged_at'])
+        df = df[(df['merged_at'].dt.date >= start_date) & (df['merged_at'].dt.date <= end_date)]
+        return df
+    else:
+        print(f"Failed to fetch PRs merged between {start_date} and {end_date}. Status code: {response.status_code} - {response.text}")
+        return None
